ac3aebb4940a2fb24a3634d97cc4cf9687a14d94
lidya <lidya. abraham@student.moringaschool.com>
2022-08-21 21:53:55 +0300

    commpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return`This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(emphatic){
+    return function(worker) {
+        return`You are ${emphatic}${worker}${emphatic}!`;
+    }
+}
+console.log(wrapAdjective('*'));
+
+console.log(wrapAdjective('||'));
